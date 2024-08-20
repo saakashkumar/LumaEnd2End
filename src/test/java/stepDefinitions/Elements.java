d9965f9a7486c886baab79aefc555c1bb8e3e03e
AAKASH SINGH <saakashkumar@gmail.com>
2024-08-21 01:57:06 +0530

    Making more reusable

--- src/test/java/stepDefinitions/Elements.java
+++ src/test/java/stepDefinitions/Elements.java
@@ -1,4 +1,26 @@
 package stepDefinitions;
 
+import io.cucumber.java.it.Ma;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public class Elements {
+
+    static WebElement CreateAccount = MainStepDefinition.driver.findElement(By.linkText("Create an Account"));
+
+    static WebElement FirstName= MainStepDefinition.driver.findElement(By.xpath("//*[@id=\"firstname\"]"));
+
+    static WebElement LastName = MainStepDefinition.driver.findElement(By.xpath("//*[@id=\"lastname\"]"));
+
+    static WebElement EmailID = MainStepDefinition.driver.findElement(By.xpath("//*[@id=\"email_address\"]"));
+
+    static WebElement Password = MainStepDefinition.driver.findElement(By.xpath("//*[@id=\"password\"]"));
+
+    static WebElement ConfirmPassword = MainStepDefinition.driver.findElement(By.xpath("//*[@id=\"password-confirmation\"]"));
+
+    static WebElement SubmitButton = MainStepDefinition.driver.findElement(By.xpath("//*[@id=\"form-validate\"]/div/div[1]/button/span"));
+
+    static WebElement ConfirmationMessage = MainStepDefinition.driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/div[2]/div/div/div"));
+
+
 }
--- src/test/java/stepDefinitions/MainStepDefinition.java
+++ src/test/java/stepDefinitions/MainStepDefinition.java
@@ -8,31 +8,36 @@ import org.openqa.selenium.WebDriver;
 
 public class MainStepDefinition {
 
-    public String browserType = "chrome";
-    public WebDriver driver = DriverFactory.selectBrowser(browserType);
+    public static String browserType = "chrome";
+    public static WebDriver driver = DriverFactory.selectBrowser(browserType);
 
 
     @Given("{string} is at Singup page of Luma")
-    public void is_at_singup_page_of_luma(String string) {
+    public void is_at_singup_page_of_luma(String string) throws InterruptedException {
         driver.manage().window().maximize();
         driver.get("https://magento.softwaretestingboard.com/");
-        driver.findElement(By.linkText("Create an Account")).click();
+        Thread.sleep(2000);
+        Elements.CreateAccount.click();
+        Thread.sleep(3000);
+        driver.getTitle();
+
 
     }
     @When("User enters all the details and Clicked on Create account buttom")
-    public void user_enters_all_the_details_and_clicked_on_create_account_buttom() {
-        driver.findElement(By.xpath("//*[@id=\"firstname\"]")).sendKeys("Aakash");
-        driver.findElement(By.xpath("//*[@id=\"lastname\"]")).sendKeys("Singh");
-        driver.findElement(By.xpath("//*[@id=\"email_address\"]")).sendKeys("aaakashkumar@gmail.com");
-        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("123@ABCxyz");
-        driver.findElement(By.xpath("//*[@id=\"password-confirmation\"]")).sendKeys("123@ABCxyz");
-        driver.findElement(By.xpath("//*[@id=\"form-validate\"]/div/div[1]/button/span")).click();
+    public void user_enters_all_the_details_and_clicked_on_create_account_buttom() throws InterruptedException {
+        Elements.FirstName.sendKeys("Aakash");
+        Elements.LastName.sendKeys("Singh");
+        Elements.EmailID.sendKeys("aaakashkumar@gmail.com");
+        Elements.Password.sendKeys("123@ABCxyz");
+        Elements.ConfirmPassword.sendKeys("123@ABCxyz");
+        Elements.SubmitButton.click();
+        Thread.sleep(2000);
 
     }
     @Then("User's account get created and message displayed.")
     public void user_s_account_get_created_and_message_displayed() {
 
-        driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/div[2]/div/div/div"));
+        Elements.ConfirmationMessage.getText();
         String pageTitle= driver.getTitle();
         if (pageTitle == "My Account"){
             System.out.println("User have successfully logged in");
